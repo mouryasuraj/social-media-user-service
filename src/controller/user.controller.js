146e9f6272ccf548ede2619cf73ae5548094e496
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-03-14 01:21:58 +0530

    feature: validate create and update user request body and save it to database

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,14 +1,23 @@
 import { profileCreated, profileUpdated } from "../config/index.js"
 import { User } from "../model/user.model.js"
 import { handleSendResponse, consoleError, handleError, AppError } from "../utils/index.js"
-import { validateCreateUserReqBody } from "../validation/user.validation.js"
+import { validateCreateUserReqBody, validateUpdateUserReqBody } from "../validation/user.validation.js"
 
 // handleCreateProfile
 export const handleCreateProfile = async (req,res) =>{
     try {
         const reqbody = validateCreateUserReqBody(req)
+        const loggedInUser = req.user
 
-        handleSendResponse(res, 201,true,profileCreated,{})
+        // Check profile already created or not
+        const existingProfile = await User.findOne({userId:loggedInUser.sub})
+        if(existingProfile) throw new AppError("Profile has been already created", 409)
+
+        // Create User Profile
+        const newProfile = new User({...reqbody, userId:loggedInUser.sub})
+        const createdUser = await newProfile.save()
+
+        handleSendResponse(res, 201,true,profileCreated,createdUser)
         
     } catch (error) {
         consoleError(error)
@@ -21,9 +30,15 @@ export const handleCreateProfile = async (req,res) =>{
 // handleUpdateProfile
 export const handleUpdateProfile = async (req,res) =>{
     try {
+        const reqBody = validateUpdateUserReqBody(req)
+        const loggedInUser = req.user
 
+        console.log(loggedInUser)
 
-        handleSendResponse(res, 201,true,profileUpdated,{})
+        // update profile
+        const user = await User.findOneAndUpdate({userId:loggedInUser.sub},{$set:reqBody},{new:true})
+
+        handleSendResponse(res, 201,true,profileUpdated,user)
         
     } catch (error) {
         consoleError(error)
@@ -42,7 +57,7 @@ export const handleGetProfile = async (req,res)=>{
         const profile = await User.findOne({userId})
         if(!profile) throw new AppError("user not found", 404)
 
-        handleSendResponse(res, 200, true, "success",userId)
+        handleSendResponse(res, 200, true, "success",profile)
     } catch (error) {
         consoleError(error)
         const status = error.statusCode || 500;
--- src/validation/user.validation.js
+++ src/validation/user.validation.js
@@ -1,5 +1,6 @@
 import { allowedUserFields, reqBodyNotPresentTxt } from "../config/index.js"
 import { AppError } from "../utils/index.js"
+import validator from 'validator'
 
 
 export const validateCreateUserReqBody = (req) =>{
@@ -24,15 +25,59 @@ export const validateCreateUserReqBody = (req) =>{
     const age = Number(reqBody?.age || 0)
 
     const createUserFieldValidation = [
-        { valid: fullName && fullName.length>100, message:"Fullname should not exceed more than 100 character" },
-        { valid: age && (age<12 || age>100), message:"Fullname should not exceed more than 100 character" },
+        { valid: fullName==="", message:"Fullname is required" },
+        { valid: fullName && fullName.trim().length>100, message:"Fullname should not exceed more than 100 character" },
+        { valid: age && (age<12 || age>100), message:"Age should be greater than 12 or less than 100" },
+        { valid: skills && skills.length>60, message:"Skill limit exceeded more than 60" },
+        { valid: about && about.trim().length>200, message:"About should be less than 200" },
+        { valid: photoUrl && validator.isURL(photoUrl) , message:"URL is not valid" },
     ]
+    
 
+    for (const check of createUserFieldValidation){
+        if(check.valid){
+            throw new AppError(check.message, 400)
+        }
+    }
 
+    reqBody.fullName = fullName.trim()
+    reqBody.about = about.trim()
+    reqBody.photoUrl = photoUrl.trim()
 
+    return reqBody
 
+}
 
+// validateUpdateUserReqBody
+export const validateUpdateUserReqBody = (req) =>{
+    if(!req.body || Object.keys(req?.body || {}).length===0) throw new AppError(reqBodyNotPresentTxt, 400)
 
-    return
+    const reqBody = req.body
+    const reqBodyFields = Object.keys(reqBody)
 
+    const extraFields = reqBodyFields.filter(d => !allowedUserFields.includes(d))
+    if(extraFields.length>0) throw new AppError(`Extra fields are not allowed: [${extraFields.join(", ")}]`)
+
+    const {fullName, skills, about, photoUrl} = reqBody
+    const age = Number(reqBody?.age || 0)
+
+    const updateUserFieldValidation = [
+        { valid: fullName && fullName.trim().length>100, message:"Fullname should not exceed more than 100 character" },
+        { valid: age && (age<12 || age>100), message:"Age should be greater than 12 or less than 100" },
+        { valid: skills && skills.length>60, message:"Skill limit exceeded more than 60" },
+        { valid: about && about.trim().length>200, message:"About should be less than 200" },
+        { valid: photoUrl && validator.isURL(photoUrl) , message:"URL is not valid" },
+    ]
+    
+    for (const check of updateUserFieldValidation){
+        if(check.valid){
+            throw new AppError(check.message, 400)
+        }
+    }
+
+    reqBody.fullName = fullName?.trim()
+    reqBody.about = about?.trim()
+    reqBody.photoUrl = photoUrl?.trim()
+
+    return reqBody
 }
